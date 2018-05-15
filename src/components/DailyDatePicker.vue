cc04c4c47fcc9bf9d7d8dff0c263526d33d877ea
164761965 <164761965@qq.com>
2018-05-15 20:39:19 +0800

    定义DailyDatePicker组件

--- src/components/DailyDatePicker.vue
+++ src/components/DailyDatePicker.vue
@@ -15,25 +15,33 @@
  * @param {function} [handleDateAdd] - 后一天的回掉函数  （参数后一天的日期  格式：yyyy-MM-dd）
  *
  * @example
- * <oms2-daily-date-picker @date-add="handleDateAdd" @date-reduce="handleDateReduce"></oms2-daily-date-picker>
+ * <oms2-daily-date-picker :date="date" @date-add="handleDateAdd" @date-reduce="handleDateReduce"></oms2-daily-date-picker>
  * 
  * 
- * <script>
- *     import DailyDatePicker from './../components/DailyDatePicker'
- *     export default{
- *         methods: {
-              handleDateAdd(date){
-                  alert("加一天:"+date)
-              },
-              handleDateReduce(date){
-                  alert("减一天:"+date)
-              }
-           },
-           components:{
-              'oms2-daily-date-picker':DailyDatePicker
-           }
- *     }
- * < /script>
+ *<script>
+  import DailyDatePicker from './../components/DailyDatePicker'
+  export default {
+    data() {
+      return {
+        //一定要给绑定date对象，不然会报错
+        date: {       //需要定义成这种对象的形式，因为父组件传值(对象)，子组件收到对象后可随意改变对象的属性，但不能改变对象本身。如果父组件传的不是对象，子组件直接修改父组件的值会报错。因此定义成这种形式
+          time: '2018-01-01'
+        }
+      }
+    },
+    methods: {
+      handleDateAdd(date) {
+        alert("加一天:" + date)
+      },
+      handleDateReduce(date) {
+        alert("减一天:" + date)
+      }
+    },
+    components: {
+      'oms2-daily-date-picker': DailyDatePicker
+    }
+  }
+ < /script>
  */
 
     import timepicker from './../assets/js/timepicker'
@@ -44,11 +52,12 @@
         name: 'DailyDatePicker',
         data() {
             return {
-                date: timepicker.startTime,
                 currentDate: getCurrentDate(),
                 option: timepicker.option,
             }
         },
+        props:['date'],
+        
         methods: {
             handleDateAdd() {
                 let compare = compareDate(this.date.time, this.currentDate)
