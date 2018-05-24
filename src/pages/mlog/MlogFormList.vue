709bbf14b433c4aa072724344704bf41974f04e4
Demon-linyi <zhengsj2014@163.com>
2018-05-24 10:07:07 +0800

    添加录井导航页及录井报表3个

---
+++ src/pages/mlog/MlogFormList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <mt-header :title="$route.meta.title" fixed>
+      <mt-button slot="left" icon="back" @click="handleBack">返回</mt-button>
+    </mt-header>
+    <router-link to="/mlogsOfRecordWell">
+      <button type="button" class="btn btn-primary">录井日报</button>
+    </router-link>
+    <br/>
+    <br/>
+    <router-link to="/MonthLogOfLogWellByCompany">
+      <button type="button" class="btn btn-primary">录井分市场月报</button>
+    </router-link>
+    <br/>
+    <br/>
+    <router-link to="/monthLogOfWellWorkload">
+      <button type="button" class="btn btn-primary">录井工作量月报</button>
+    </router-link>
+  </div>
+</template>
+
+<script>
+    export default {
+
+        methods: {
+          handleBack() {
+            window.history.length > 1 ?
+              this.$router.go(-1) :
+              this.$router.push('/')
+          }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
