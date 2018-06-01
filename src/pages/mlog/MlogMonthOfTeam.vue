63728d49f5ed0d13e1f0c02aead0ae8719cd1e95
Demon-linyi <zhengsj2014@163.com>
2018-06-01 14:22:27 +0800

    录井队伍统计数据不存在标红处理

--- src/pages/mlog/MlogMonthOfTeam.vue
+++ src/pages/mlog/MlogMonthOfTeam.vue
@@ -83,21 +83,68 @@
               if(data.body){
                 this.tableData=data.body
                 //检查是否含有所有地区jujorgName
-                let jujorgNames=['集团总计','大庆钻探','西部钻探','长城钻探','渤海钻探','川庆钻探','中油测井']
-                let jujisFromDate=[]
-                for(var date of this.tableData){
-                  jujisFromDate.push(date.jujorgName)
+                let jujorgNames=[{'jujorgName':'集团总计','cjorgName':''},
+                  {'jujorgName':'大庆钻探','cjorgName':'小计'},
+                  {'jujorgName':'大庆钻探','cjorgName':'录井一公司'},
+                  {'jujorgName':'大庆钻探','cjorgName':'录井二公司'},
+                  {'jujorgName':'西部钻探','cjorgName':'小计'},
+                  {'jujorgName':'西部钻探','cjorgName':'吐哈录井工程公司'},
+                  {'jujorgName':'西部钻探','cjorgName':'克拉玛依录井工程公司'},
+                  {'jujorgName':'长城钻探','cjorgName':'小计'},
+                  {'jujorgName':'长城钻探','cjorgName':'录井公司'},
+                  {'jujorgName':'渤海钻探','cjorgName':'小计'},
+                  {'jujorgName':'渤海钻探','cjorgName':'第一录井公司'},
+                  {'jujorgName':'渤海钻探','cjorgName':'第二录井公司'},
+                  {'jujorgName':'川庆钻探','cjorgName':'小计'},
+                  {'jujorgName':'川庆钻探','cjorgName':'地研院'},
+                  {'jujorgName':'中油测井','cjorgName':'小计'},
+                  {'jujorgName':'中油测井','cjorgName':'长庆分公司'},
+                ]
+                //let jujorgNames=['集团总计','大庆钻探','西部钻探','长城钻探','渤海钻探','川庆钻探','中油测井']
+                let jujorgNameFromDate=[]
+                for (var date of this.tableData) {
+                  jujorgNameFromDate.push({
+                    'jujorgName':date.jujorgName,
+                    'cjorgName':date.cjorgName
+                  })
                 }
-                for(var jujorgName of jujorgNames){
-                  if(!jujisFromDate.includes(jujorgName)){
+                let include=false;  //判断是否存在
+                for(var jujorgNa of jujorgNames) {
+                  include = false;
+                  for (var juji of jujorgNameFromDate) {
+                    if (juji.jujorgName == jujorgNa.jujorgName && juji.cjorgName == jujorgNa.cjorgName) {
+                      include = true
+                    }
+                  }
+                  if (!include) {
                     //不存在需要添加
-                    let newItem={
-                      jujorgName:jujorgName,
-                      remark:'Not exist'
+                    let newItem = {
+                      'jujorgName': jujorgNa.jujorgName,
+                      'cjorgName': jujorgNa.cjorgName,
+                      remark: 'Not exist'
                     }
                     this.tableData.push(newItem)
                   }
                 }
+                //排序
+                this.tableData.sort((pre,next)=>{
+                  let preIndex=-1
+                  let nextIndex=-1
+                  let index=0;
+                  for(var item of jujorgNames){
+                    if(item.jujorgName==pre.jujorgName&&item.cjorgName==pre.cjorgName){
+                      preIndex=index
+                    }
+                    if(item.jujorgName==next.jujorgName&&item.cjorgName==next.cjorgName){
+                      nextIndex=index
+                    }
+                    if(preIndex!=-1&&nextIndex!=-1){
+                      break
+                    }
+                    index++
+                  }
+                  return preIndex-nextIndex
+                })
                 //排序
                 // this.tableData.sort((pre,next)=>{
                 //   let preIndex=jujorgNames.indexOf(pre.jujorgName)
@@ -124,44 +171,48 @@
           handleChange(){
             this.requestData()
           },
-          // //设置列单元格样式
-           cellMerge(rowIndex,rowData,field){
-             if (field === 'jujorgName' && rowData[field] === '集团总计') {
-               return {
-                 colSpan: 2,
-                 rowSpan: 1,
-                 content: '集团总计',
-                 componentName: ''
-               }
+          //设置列单元格样式
+         cellMerge(rowIndex,rowData,field){
+           if (field === 'jujorgName' && rowData[field] === '集团总计') {
+             return {
+               colSpan: 2,
+               rowSpan: 1,
+               content: '集团总计',
+               componentName: ''
              }
-             if(field==='jujorgName') {
-               let jujorgName = rowData[field]
-               let jujorgNameList = []
-               if (jujorgNameList.indexOf(jujorgName) == -1) {
-                 //还没有合并该局级单位
-                 let num = 0  //记录这个局级单位有多少个
-                 for (let i = rowIndex; i < this.tableData.length; i++) {
-                   if (this.tableData[i].jujorgName == jujorgName) {
-                     num++
-                   }
+           }
+           if(field==='jujorgName') {
+             let jujorgName = rowData[field]
+             let jujorgNameList = []
+             if (jujorgNameList.indexOf(jujorgName) == -1) {
+               //还没有合并该局级单位
+               let num = 0  //记录这个局级单位有多少个
+               for (let i = rowIndex; i < this.tableData.length; i++) {
+                 if (this.tableData[i].jujorgName == jujorgName) {
+                   num++
                  }
-                 if (num > 0) {
-                   return {
-                     colSpan: 1,
-                     rowSpan: num,
-                     content: jujorgName,
-                     componentName: ''
-                   }
-                 }
-                 jujorgNameList.push(jujorgName)
                }
+               if (num > 0) {
+                 return {
+                   colSpan: 1,
+                   rowSpan: num,
+                   content: jujorgName,
+                   componentName: ''
+                 }
+               }
+               jujorgNameList.push(jujorgName)
              }
-          },
-          columnCellClass(rowIndex,columnName,rowData){
-            if(columnName=='jujorgName'&&this.tableData[rowIndex].remark=='Not exist'){
-              return 'oms2-item-not-exict'
-            }
+           }
+        },
+        columnCellClass(rowIndex, columnName, rowData) {
+          if(columnName=='jujorgName'&&this.tableData[rowIndex].remark=='Not exist'){
+            return 'oms2-item-not-exict'
           }
+          if(columnName=='cjorgName'&&this.tableData[rowIndex].remark=='Not exist'){
+            return 'oms2-item-not-exict'
+          }
+
+        },
         },
         components: {
           'oms2-date-picker-monthly':DatePickerMonthly
