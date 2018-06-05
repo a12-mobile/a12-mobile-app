479fc6ceb9cbcfff5f70097f47c240d8657c7007
Demon-linyi <zhengsj2014@163.com>
2018-06-05 16:53:02 +0800

    新增测井工作量日报取数据方法

--- src/service/log/logGetData.js
+++ src/service/log/logGetData.js
@@ -1,11 +1,21 @@
 import httpService from './../http/httpService'
 
 /**
- * 向服务器获取钻井重点井日报数据
- * @param {*} date 
+ * 向服务器获取测井工作量月报
+ * @param {*} date
  */
 export function getMonthlyOfWorkload(date) {
   return httpService('GET', '/log/monthly/workload', {
+    'date': date
+  })
+}
+
+/**
+ * 向服务器获取测井工作量日报
+ * @param {*} date
+ */
+export function getDailyOfWorkload(date) {
+  return httpService('GET', '/log/daily/findWorkload', {
     'date': date
   })
 }
