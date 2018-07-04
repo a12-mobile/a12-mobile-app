c010a9feb293ba473f5abaf384f2daed5f755785
Demon-linyi <zhengsj2014@163.com>
2018-07-04 20:46:43 +0800

    更改样式

--- src/pages/drill/DrillFormList.vue
+++ src/pages/drill/DrillFormList.vue
@@ -3,71 +3,71 @@
     <div class="container">
 
       <div class="row">
-        <div class="col-4 oms2-grid-cell">
+        <div class="col-4 oms2-g-grid-cell">
           <router-link to="/well/list">
-            <img class='oms2-grid-cell-image' src="./../../assets/icon/wellList.png">
+            <img class='oms2-g-grid-cell-image' src="./../../assets/icon/wellList.png">
           </router-link>
           <p>井列表</p>
         </div>
-        <div class="col-4 oms2-grid-cell">
+        <div class="col-4 oms2-g-grid-cell">
           <router-link to="/well/list/key">
-            <img class='oms2-grid-cell-image' src="./../../assets/icon/careWellList.png">
+            <img class='oms2-g-grid-cell-image' src="./../../assets/icon/careWellList.png">
           </router-link>
           <p>关注井列表</p>
         </div>
-        <div class="col-4 oms2-grid-cell">
+        <div class="col-4 oms2-g-grid-cell">
           <router-link to="/real-time/list/project">
-            <img class='oms2-grid-cell-image' src="./../../assets/icon/realtimeData.png">
+            <img class='oms2-g-grid-cell-image' src="./../../assets/icon/realtimeData.png">
           </router-link>
           <p>实时数据监测</p>
         </div>
-        <div class="col-4 oms2-grid-cell">
+        <div class="col-4 oms2-g-grid-cell">
           <!-- <a href="http://11.10.97.109:9493/websocket/echarts/demo.html"> -->
           <a :href="dataMonitor">
-            <img class='oms2-grid-cell-image' src="./../../assets/icon/realtimeCharts.png">
+            <img class='oms2-g-grid-cell-image' src="./../../assets/icon/realtimeCharts.png">
           </a>
           <p>实时曲线监测</p>
         </div>
-          <div class="col-4 oms2-grid-cell">
+          <div class="col-4 oms2-g-grid-cell">
             <router-link to="/drill/daily/well/key">
-              <img class='oms2-grid-cell-image' src="./../../assets/icon/drill/dailyOfKeyWell.png">
+              <img class='oms2-g-grid-cell-image' src="./../../assets/icon/drill/dailyOfKeyWell.png">
             </router-link>
             <p>重点井日报</p>
           </div>
 
-          <div class="col-4 oms2-grid-cell">
+          <div class="col-4 oms2-g-grid-cell">
             <router-link to="/dRDailyOfRigDynamic">
-              <img class='oms2-grid-cell-image' src="./../../assets/icon/dh/monthlyOfWorkload.png">
+              <img class='oms2-g-grid-cell-image' src="./../../assets/icon/dh/monthlyOfWorkload.png">
             </router-link>
             <p>钻机动态日报</p>
           </div>
-          <div class="col-4 oms2-grid-cell">
+          <div class="col-4 oms2-g-grid-cell">
             <router-link to="/dailyOfDrilling">
-              <img class='oms2-grid-cell-image' src="./../../assets/icon/wellList.png">
+              <img class='oms2-g-grid-cell-image' src="./../../assets/icon/wellList.png">
             </router-link>
             <p>工作量日报</p>
           </div>
-          <div class="col-4 oms2-grid-cell">
+          <div class="col-4 oms2-g-grid-cell">
             <router-link to="/years/rigstatistics">
-              <img class='oms2-grid-cell-image' src="./../../assets/icon/log/workloadOfBlock.png">
+              <img class='oms2-g-grid-cell-image' src="./../../assets/icon/log/workloadOfBlock.png">
             </router-link>
             <p>钻机统计表(年报)</p>
           </div>
-          <div class="col-4 oms2-grid-cell">
+          <div class="col-4 oms2-g-grid-cell">
             <router-link to="/years/drillTeam">
-              <img class='oms2-grid-cell-image' src="./../../assets/icon/log/teamOfTeamType.png">
+              <img class='oms2-g-grid-cell-image' src="./../../assets/icon/log/teamOfTeamType.png">
             </router-link>
             <p>钻井队伍分布表(年报)</p>
           </div>
-          <div class="col-4 oms2-grid-cell">
+          <div class="col-4 oms2-g-grid-cell">
             <router-link to="/years/footage">
-              <img class='oms2-grid-cell-image' src="./../../assets/icon/log/teamOfDeviceType.png">
+              <img class='oms2-g-grid-cell-image' src="./../../assets/icon/log/teamOfDeviceType.png">
             </router-link>
             <p>钻井工作量-钻井进尺表(年报)</p>
           </div>
-          <div class="col-4 oms2-grid-cell">
+          <div class="col-4 oms2-g-grid-cell">
             <router-link to="/years/drillNum">
-              <img class='oms2-grid-cell-image' src="./../../assets/icon/log/foreignOfTeamType.png">
+              <img class='oms2-g-grid-cell-image' src="./../../assets/icon/log/foreignOfTeamType.png">
             </router-link>
             <p>钻井工作量-开钻口数表(年报)</p>
           </div>
--- src/pages/log/LogFormList.vue
+++ src/pages/log/LogFormList.vue
@@ -2,45 +2,45 @@
   <div id="LogFormList">
     <div class="container">
       <div class="row">
-        <div class="col-4 oms2-grid-cell">
+        <div class="col-4 oms2-g-grid-cell">
           <router-link to="/log/daily/workload">
-            <img class='oms2-grid-cell-image' src="./../../assets/icon/log/dailyOfWorkload.png">
+            <img class='oms2-g-grid-cell-image' src="./../../assets/icon/log/dailyOfWorkload.png">
           </router-link>
           <p>工作量日报</p>
         </div>
-        <div class="col-4 oms2-grid-cell">
+        <div class="col-4 oms2-g-grid-cell">
           <router-link to="/log/Monthly/workload">
-            <img class='oms2-grid-cell-image' src="./../../assets/icon/log/monthlyOfWorkload.png">
+            <img class='oms2-g-grid-cell-image' src="./../../assets/icon/log/monthlyOfWorkload.png">
           </router-link>
           <p>工作量月报</p>
         </div>
-        <div class="col-4 oms2-grid-cell">
+        <div class="col-4 oms2-g-grid-cell">
           <router-link to="/log/Monthly/workloadOfBlock">
-            <img class='oms2-grid-cell-image' src="./../../assets/icon/log/workloadOfBlock.png">
+            <img class='oms2-g-grid-cell-image' src="./../../assets/icon/log/workloadOfBlock.png">
           </router-link>
           <p>工作量油区分布</p>
         </div>
-        <div class="col-4 oms2-grid-cell">
+        <div class="col-4 oms2-g-grid-cell">
           <router-link to="/log/Monthly/TeamByTeamType">
-            <img class='oms2-grid-cell-image' src="./../../assets/icon/log/teamOfTeamType.png">
+            <img class='oms2-g-grid-cell-image' src="./../../assets/icon/log/teamOfTeamType.png">
           </router-link>
           <p>专业队伍分布表(按队伍类型)</p>
         </div>
-        <div class="col-4 oms2-grid-cell">
+        <div class="col-4 oms2-g-grid-cell">
           <router-link to="/log/Monthly/TeamByDeviceType">
-            <img class='oms2-grid-cell-image' src="./../../assets/icon/log/teamOfDeviceType.png">
+            <img class='oms2-g-grid-cell-image' src="./../../assets/icon/log/teamOfDeviceType.png">
           </router-link>
           <p>专业队伍分布表(按设备类型)</p>
         </div>
-        <div class="col-4 oms2-grid-cell">
+        <div class="col-4 oms2-g-grid-cell">
           <router-link to="/log/Monthly/ForeignTeamByTeamType">
-            <img class='oms2-grid-cell-image' src="./../../assets/icon/log/foreignOfTeamType.png">
+            <img class='oms2-g-grid-cell-image' src="./../../assets/icon/log/foreignOfTeamType.png">
           </router-link>
           <p>专业队伍国外分布表(按队伍类型)</p>
         </div>
-        <div class="col-4 oms2-grid-cell">
+        <div class="col-4 oms2-g-grid-cell">
           <router-link to="/log/Monthly/ForeignTeamByDeviceType">
-            <img class='oms2-grid-cell-image' src="./../../assets/icon/log/foreignOfDeviceType.png">
+            <img class='oms2-g-grid-cell-image' src="./../../assets/icon/log/foreignOfDeviceType.png">
           </router-link>
           <p>专业队伍国外分布表(按设备类型)</p>
         </div>
